b0c8b08cf6ffe49334aaa82bb2a514daf5619049
David Higgins <getdavidhiggins@gmail.com>
2013-08-11 17:18:17 +0100

    _

---
+++ data.php
@@ -0,0 +1,7 @@
+<?php
+echo "var json".$type." = ";
+
+$a = file_get_contents("http://eire-media.com/gauge/?type=".$type);
+
+echo $a;
+?>
---
+++ index.php
@@ -0,0 +1,27 @@
+<?php
+
+$headers = array(
+    'X-Gauges-Token: {your token goes here}'
+);
+        // create curl resource
+        $ch = curl_init();
+        
+
+        // set url
+        curl_setopt($ch, CURLOPT_URL, "https://secure.gaug.es/gauges/{site-id-goes-here-to-find-it-go-to-the-dashboard-and-see-it-in-the-URL}".$type);
+        
+        // headers
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+        
+
+        //return the transfer as a string
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+
+        // $output contains the output string
+        $output = curl_exec($ch);
+        
+        echo $output ;
+
+        // close curl resource to free up system resources
+        curl_close($ch);     
+?>
